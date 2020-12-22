rkboni/openwrt

commit 44e237f58fc5887cbaa0c442eaefde8812ff4f41

tegra: sysupgrade: use get_image_dd wrapper
This function eliminates false-positive errors emitted by dd.

Signed-off-by: Tomasz Maciej Nowak <tmn505@gmail.com>
[drop argument check changes]
Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/tegra/base-files/lib/upgrade/platform.sh
@@ -13,7 +13,7 @@ platform_check_image() {
 	get_partitions "/dev/$diskdev" bootdisk
 
 	#extract the boot sector from the image
-	get_image "$@" | dd of=/tmp/image.bs count=1 bs=512b 2>/dev/null
+	get_image_dd "$1" of=/tmp/image.bs count=1 bs=512b
 
 	get_partitions /tmp/image.bs image
 
@@ -53,7 +53,7 @@ platform_do_upgrade() {
 		get_partitions "/dev/$diskdev" bootdisk
 
 		#extract the boot sector from the image
-		get_image "$@" | dd of=/tmp/image.bs count=1 bs=512b
+		get_image_dd "$1" of=/tmp/image.bs count=1 bs=512b
 
 		get_partitions /tmp/image.bs image
 
@@ -64,7 +64,7 @@ platform_do_upgrade() {
 	fi
 
 	if [ -n "$diff" ]; then
-		get_image "$@" | dd of="/dev/$diskdev" bs=4096 conv=fsync
+		get_image_dd "$1" of="/dev/$diskdev" bs=4096 conv=fsync
 
 		# Separate removal and addtion is necessary; otherwise, partition 1
 		# will be missing if it overlaps with the old partition 2
@@ -75,12 +75,12 @@ platform_do_upgrade() {
 	fi
 
 	#write uboot image
-	get_image "$@" | dd of="$diskdev" bs=512 skip=1 seek=1 count=4097 conv=fsync,notrunc
+	get_image_dd "$1" of="$diskdev" bs=512 skip=1 seek=1 count=4097 conv=fsync,notrunc
 	#iterate over each partition from the image and write it to the boot disk
 	while read part start size; do
 		if export_partdevice partdev $part; then
 			echo "Writing image to /dev/$partdev..."
-			get_image "$@" | dd of="/dev/$partdev" ibs="512" obs=1M skip="$start" count="$size" conv=fsync
+			get_image_dd "$1" of="/dev/$partdev" ibs="512" obs=1M skip="$start" count="$size" conv=fsync
 		else
 			echo "Unable to find partition $part device, skipped."
 		fi
@@ -88,5 +88,5 @@ platform_do_upgrade() {
 
 	#copy partition uuid
 	echo "Writing new UUID to /dev/$diskdev..."
-	get_image "$@" | dd of="/dev/$diskdev" bs=1 skip=440 count=4 seek=440 conv=fsync
+	get_image_dd "$1" of="/dev/$diskdev" bs=1 skip=440 count=4 seek=440 conv=fsync
 }

commit e8fe717239133fef3c7a66c3492caf843d6b273a

tegra: sysupgrade: remove check for number of arguments
This chunk got mistakenly removed from 44e237f, since the get_image_dd
evaluates only first agument, so that check is useless.

Fixes: 44e237f ("tegra: sysupgrade: use get_image_dd wrapper")
Signed-off-by: Tomasz Maciej Nowak <tmn505@gmail.com>

target/linux/tegra/base-files/lib/upgrade/platform.sh
@@ -3,8 +3,6 @@ REQUIRE_IMAGE_METADATA=1
 platform_check_image() {
 	local diskdev partdev diff
 
-	[ "$#" -gt 1 ] && return 1
-
 	export_bootdevice && export_partdevice diskdev 0 || {
 		v "Unable to determine upgrade device"
 		return 1