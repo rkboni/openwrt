c984314fb51c3d7caef99ab392315f05865b7ff1
Rafal Boni <rafal.boni@gmail.com>
2024-10-24 17:41:28 -0400

    Add workflow / build scripts for building ipq40xx/ipq806x Meraki targets.
    (cherry picked from commit aed77c4dc0d7fcd7a75c8d5866d8f1e60728e510)

---
+++ genconfig-ipq806x.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+arch=ipq806x
+target=generic
+version=snapshots
+wget https://downloads.openwrt.org/${version}/targets/${arch}/${target}/config.buildinfo -O config.buildinfo
+cat config.buildinfo | grep -v CONFIG_TARGET_DEVICE_ | grep -v CONFIG_TARGET_ALL_PROFILES | grep -v CONFIG_TARGET_MULTI_PROFILE > .config
+echo CONFIG_TARGET_ALL_PROFILES=n >> .config
+echo CONFIG_TARGET_MULTI_PROFILE=n >> .config
+echo CONFIG_TARGET_DEVICE_${arch}_generic_DEVICE_meraki_mr42=y >> .config
+echo CONFIG_TARGET_DEVICE_${arch}_generic_DEVICE_meraki_mr52=y >> .config
+echo CONFIG_TARGET_DEVICE_PACKAGES_${arch}_generic_DEVICE_meraki_mr42=\"\" >> .config
+echo CONFIG_TARGET_DEVICE_PACKAGES_${arch}_generic_DEVICE_meraki_mr52=\"\" >> .config
+
+#add luci
+echo CONFIG_PACKAGE_luci=y >> .config
+make defconfig
