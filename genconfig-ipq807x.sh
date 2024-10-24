428d851dd2db96349b5b9724de68ddcad78710c6
Rafal Boni <rafal.boni@gmail.com>
2024-10-24 17:41:28 -0400

    Add workflow / build scripts for building ipq40xx/ipq806x Meraki targets.

---
+++ genconfig-ipq807x.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+arch=qualcommax
+target=ipq807x
+version=releases/25.12-SNAPSHOT
+wget https://downloads.openwrt.org/${version}/targets/${arch}/${target}/config.buildinfo -O config.buildinfo
+cat config.buildinfo | grep -v CONFIG_TARGET_DEVICE_ | grep -v CONFIG_TARGET_ALL_PROFILES | grep -v CONFIG_TARGET_MULTI_PROFILE > .config
+echo CONFIG_TARGET_ALL_PROFILES=n >> .config
+echo CONFIG_TARGET_MULTI_PROFILE=n >> .config
+echo CONFIG_TARGET_DEVICE_qualcommax_ipq807x_DEVICE_linksys_homewrk=y >> .config
+echo CONFIG_TARGET_DEVICE_PACKAGES_qualcommax_ipq807x_DEVICE_linksys_homewrk="" >> .config
+echo CONFIG_TARGET_DEVICE_qualcommax_ipq807x_DEVICE_linksys_mx4200v1=y >> .config
+echo CONFIG_TARGET_DEVICE_PACKAGES_qualcommax_ipq807x_DEVICE_linksys_mx4200v1="" >> .config
+echo CONFIG_TARGET_DEVICE_qualcommax_ipq807x_DEVICE_linksys_mx4200v2=y >> .config
+echo CONFIG_TARGET_DEVICE_PACKAGES_qualcommax_ipq807x_DEVICE_linksys_mx4200v2="" >> .config
+echo CONFIG_TARGET_DEVICE_qualcommax_ipq807x_DEVICE_linksys_mx4300=y >> .config
+echo CONFIG_TARGET_DEVICE_PACKAGES_qualcommax_ipq807x_DEVICE_linksys_mx4300="" >> .config
+
+#add luci
+echo CONFIG_PACKAGE_luci=y >> .config
+make defconfig
