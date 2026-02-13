rkboni/openwrt

commit 74704015884a58256998404d53c1ab928d26d210

qualcommbe: ipq95xx: Add initial support for new target
Add initial support for new target with the initial patch for ethernet
support using pending upstream patches for PCS UNIPHY, PPE and EDMA.

Only initramfs currently working as support for new SPI/NAND
implementation, USB, CPUFreq and other devices is still unfinished and
needs to be evaluated.

Link: https://github.com/openwrt/openwrt/pull/17725
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

target/linux/qualcommbe/ipq95xx/base-files/lib/upgrade/platform.sh
@@ -1,0 +1,12 @@
+PART_NAME=firmware
+
+RAMFS_COPY_BIN='fw_printenv fw_setenv head'
+RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
+
+platform_do_upgrade() {
+	case "$(board_name)" in
+	*)
+		default_do_upgrade "$1"
+		;;
+	esac
+}

commit 2c93b2d0ec2ab8352e39eaac1f3c017dfa572d44

qualcommbe: ipq95xx: require image metadata for sysupgrade checks
Add the flag required to enforce the fwtool's image metadata checks.
All sysupgrade image recipes on this platform already append the metadata.

Fixes: 74704015884a58256998404d53c1ab928d26d210 ("qualcommbe: ipq95xx: Add initial support for new target")
Signed-off-by: Mario Andrés Pérez <mapb_@outlook.com>
Link: https://github.com/openwrt/openwrt/pull/22010
Signed-off-by: Robert Marko <robimarko@gmail.com>

target/linux/qualcommbe/ipq95xx/base-files/lib/upgrade/platform.sh
@@ -1,4 +1,5 @@
 PART_NAME=firmware
+REQUIRE_IMAGE_METADATA=1
 
 RAMFS_COPY_BIN='fw_printenv fw_setenv head'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'