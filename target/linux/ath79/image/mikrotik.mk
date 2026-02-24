rkboni/openwrt

commit 707bb8ca8489b78dbcfbd80aa67ee6f2fddf60ff

ath79: remove kmod-ath9k and wpad from Mikrotik RB750r2
The RB750r2 (HEXLite) does not have wifi and those packages bloat the image
by a significant amount. When building a custom image with WireGuard and
booting that from initramfs, there wasn't enough space left in tmpfs to
upload and flash the squashfs image. Investigating what packages I could
remove, I discovered these unneeded ones.

Signed-off-by: Eicke Herbertz <wolletd@posteo.de>
Link: https://github.com/openwrt/openwrt/pull/22134
Signed-off-by: Robert Marko <robimarko@gmail.com>

target/linux/ath79/image/mikrotik.mk
@@ -24,6 +24,7 @@ define Device/mikrotik_routerboard-750-r2
   $(Device/mikrotik_nor)
   SOC := qca9533
   DEVICE_MODEL := RouterBOARD 750 r2 (hEX lite)
+  DEVICE_PACKAGES := -kmod-ath9k -wpad-basic-mbedtls
   IMAGE_SIZE := 16256k
   SUPPORTED_DEVICES += rb-750-r2
 endef

commit f6d5015b89613ef278615df89476dd450acb00b6

ath79: mikrotik: fix DEVICE_PACKAGES
Instead of redifining the DEVICE_PACKAGES, make sure to expand the list
so yafut is included by default as well.

Fixes: 707bb8ca8489 ("ath79: remove kmod-ath9k and wpad from Mikrotik RB750r2")
Signed-off-by: Robert Marko <robimarko@gmail.com>

target/linux/ath79/image/mikrotik.mk
@@ -24,7 +24,7 @@ define Device/mikrotik_routerboard-750-r2
   $(Device/mikrotik_nor)
   SOC := qca9533
   DEVICE_MODEL := RouterBOARD 750 r2 (hEX lite)
-  DEVICE_PACKAGES := -kmod-ath9k -wpad-basic-mbedtls
+  DEVICE_PACKAGES += -kmod-ath9k -wpad-basic-mbedtls
   IMAGE_SIZE := 16256k
   SUPPORTED_DEVICES += rb-750-r2
 endef