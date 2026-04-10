c9ee12737f3d50aa25c3fb10d01db9089792d889
Rafal Boni <rafal.boni@gmail.com>
2026-04-09 20:23:11 -0400

    Add MR-53 build, some packages & kmods useful on test builds.

--- genconfig-ipq806x.sh
+++ genconfig-ipq806x.sh
@@ -9,9 +9,27 @@ echo CONFIG_TARGET_ALL_PROFILES=n >> .config
 echo CONFIG_TARGET_MULTI_PROFILE=n >> .config
 echo CONFIG_TARGET_DEVICE_${arch}_generic_DEVICE_meraki_mr42=y >> .config
 echo CONFIG_TARGET_DEVICE_${arch}_generic_DEVICE_meraki_mr52=y >> .config
+echo CONFIG_TARGET_DEVICE_${arch}_generic_DEVICE_meraki_mr53=y >> .config
 echo CONFIG_TARGET_DEVICE_PACKAGES_${arch}_generic_DEVICE_meraki_mr42=\"\" >> .config
 echo CONFIG_TARGET_DEVICE_PACKAGES_${arch}_generic_DEVICE_meraki_mr52=\"\" >> .config
+echo CONFIG_TARGET_DEVICE_PACKAGES_${arch}_generic_DEVICE_meraki_mr53=\"\" >> .config
 
-#add luci
+#add luci, tcpdump, iperf3 & some kmods
 echo CONFIG_PACKAGE_luci=y >> .config
+echo CONFIG_PACKAGE_iperf3=y >> .config
+echo CONFIG_PACKAGE_tcpdump=y >> .config
+echo CONFIG_PACKAGE_mdio-tools=y >> .config
+echo CONFIG_PACKAGE_kmod-of-mdio=y >> .config
+echo CONFIG_PACKAGE_kmod-eeprom-at24=y >> .config
+echo CONFIG_PACKAGE_kmod-gpio-button-hotplug=y >> .config
+echo CONFIG_PACKAGE_kmod-hwmon-ina2xx=y >> .config
+echo CONFIG_PACKAGE_kmod-leds-gpio=y >> .config
+echo CONFIG_PACKAGE_kmod-ledtrigger-netdev=y >> .config
+echo CONFIG_PACKAGE_kmod-nft-offload=y >> .config
+echo CONFIG_PACKAGE_kmod-phy-aquantia=y >> .config
+echo CONFIG_PACKAGE_kmod-phy-at803x=y >> .config
+echo CONFIG_PACKAGE_kmod-phy-proxy=y >> .config
+echo CONFIG_PACKAGE_kmod-phylib-qcom=y >> .config
+echo CONFIG_PACKAGE_kmod-phylink=y >> .config
+echo CONFIG_PACKAGE_kmod-qca85xx-sw=y >> .config
 make defconfig
